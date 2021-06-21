04a429262e51d52f0ee86b8852b3b8ea4e14c216
Mainul-Fahim <36801303+Mainul-Fahim@users.noreply.github.com>
2021-06-21 19:04:04 +0600

    updated

--- index.js
+++ index.js
@@ -20,13 +20,51 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-    console.log('connection error', err);
+  console.log('connection error', err);
+  const employersCollection = client.db("jobCompanion").collection("employers");
   const jobsCollection = client.db("jobCompanion").collection("jobs");
 
   console.log('Database Connected Successfully');
   // perform actions on the collection object
+
+  app.post('/addEmployers', (req, res) => {
+    const employerBook = req.body;
+    console.log(employerBook);
+    employersCollection.insertOne(employerBook)
+      .then(result => {
+        res.send(result.insertedCount > 0)
+      })
+  })
+
+  app.post('/addJobs', (req, res) => {
+    const newJob = req.body;
+    console.log('new Service', newJob);
+    jobsCollection.insertOne(newJob)
+        .then(result => {
+            console.log('insertedCount', result);
+            res.send(result)
+        })
+})
+
+app.get('/jobs', (req, res) => {
+  jobsCollection.find()
+      .toArray((err, items) => {
+          res.send(items)
+      })
+})
+
+app.post('/isEmployer', (req, res) => {
+  const email = req.body.email;
+  console.log('new Service', email);
+  employersCollection.find({email:email})
+  .toArray((err,items)=>{
+      res.send(items.length>0)
+    })
+})
+
   //client.close();
 });
+
 
 
 
